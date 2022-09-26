dcae04f47cc3dbe9473082a8795c4a681f056d3a
Chen <weifengchen198@gmial.com>
2022-09-26 17:10:14 +0800

    shiro学习代码

---
+++ hello-shrio/src/test/java/com/chen/TestExampleServlet.java
@@ -0,0 +1,14 @@
+package com.chen;
+
+import junit.framework.TestCase;
+
+/**
+ * Testing {@link com.chen.ExampleMacro}
+ */
+public class TestExampleServlet extends TestCase
+{
+    public void testBasic()
+    {
+        // add test here...
+    }
+}
